9074f4da9882714a9eae1d8324c4671f0c406c55
Morad Arafa <olenshop.sa@gmail.com>
2026-07-14 22:38:00 +0300

    Deploy: live Pomodoro dial + SOS timer animations (source ed58ef6)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "137884886" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1083872457" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
