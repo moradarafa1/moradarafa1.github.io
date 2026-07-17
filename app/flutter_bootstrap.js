afab4da83038b00d7fda331f7c5e7215bb3b2894
Morad Arafa <olenshop.sa@gmail.com>
2026-07-17 19:38:04 +0300

    Deploy: prayer-times engine (offline adhan + GPS/city + editable times + pre-alerts + adhkar), consent notice rendered, Arabic wording fixes

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "940926822" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2733876494" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
